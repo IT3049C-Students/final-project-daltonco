c0efbaca7b7f2a86fb6e841df09c31643e1c8d8e
daltonco <daltonco@mail.uc.edu>
2022-08-03 14:48:09 -0400

    Loading original RPS

---
+++ resources/javascript/index.js
@@ -0,0 +1,49 @@
+// Elements
+const welcomeScreen = document.getElementById('welcome-screen');
+const gameScreen = document.getElementById('game-screen');
+const startGameButton = document.getElementById('start-game-button');
+const userName = document.getElementById('username');
+const userSelection = document.getElementById('user-selection');
+const goButton = document.getElementById('go-button');
+const scoreParagraph = document.getElementById('score');
+const gameHistoryParagraph = document.getElementById('game-history');
+
+// instantiate the game object from the `RockPaperScissors` class.
+let game;
+
+// hide game screen
+gameScreen.classList.add('d-none');
+
+// updateScoreTallyUI
+function updateScoreTallyUI(){
+  scoreParagraph.innerText = game.username + ": " + game.score.user + " v CPU: " + game.score.cpu;
+}
+
+// updateGameHistoryUI
+function updateGameHistoryUI(){
+  gameHistoryParagraph.innerText = '';
+  for(let i = 0; i < game.gameHistoryLog.length; i++){
+    gameHistoryParagraph.innerText += game.gameHistoryLog[i] + "\n";
+  }
+}
+
+// start-game-button EventListener
+startGameButton.addEventListener('click', function () {
+  const username = userName.value;
+  game = new SuperRockPaperScissors(username);
+  welcomeScreen.classList.add('d-none');
+  gameScreen.classList.remove('d-none');
+  // Complete
+});
+
+// go-button EventListener
+goButton.addEventListener('click', function () {
+  game.play(userSelection.value);
+  updateScoreTallyUI();
+  updateGameHistoryUI();
+});
+
+// If you're doing the extra-credit, uncomment the below: reset-game-button
+// resetGameButton.addEventListener(`click`, function(e) { 
+  
+// });
